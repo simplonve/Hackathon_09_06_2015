dc29758c5785326b1819f4f85b5dc47632a6d12f
hitman07 <david.malosse@voila.fr>
2015-06-09 11:49:34 +0200

    génération du flop  refactorisé poker.rb

--- jeu_poker/poker.rb
+++ jeu_poker/poker.rb
@@ -1,23 +1,17 @@
 def poker()
   paquet = []
-	couleur_hash = {0 => "coeur", 1 => "carreau", 2 => "pique", 3 => "trefle"}
-	(0..3).each {|couleur| (2..14).each {|carte|
-	  paquet << [carte, couleur_hash[couleur]]}
-	}
-  puts paquet.length
-	cartes_restantes = 53
-  puts carte_tiree = rand(cartes_restantes)
-  puts paquet[carte_tiree]
-  flop_carte_1 = paquet[carte_tiree]
+  couleur_hash = {0 => "coeur", 1 => "carreau", 2 => "pique", 3 => "trefle"}
+  (0..3).each {|couleur| (2..14).each {|carte|
+    paquet << [carte, couleur_hash[couleur]]}
+  }
+  cartes_restantes = 53
+  carte_tiree = rand(cartes_restantes)
+  flop = []
+  (0..2).each {|flop_carte|
+  flop << paquet[carte_tiree]
   paquet.delete_at(carte_tiree)
   cartes_restantes -= 1
-  puts carte_tiree = rand(cartes_restantes)
-  flop_carte_2 = paquet[carte_tiree]
-  paquet.delete_at(carte_tiree)
-  cartes_restantes -= 1
-  puts carte_tiree = rand(cartes_restantes)
-  flop_carte_3 = paquet[carte_tiree]
-  paquet.delete_at(carte_tiree)
-  cartes_restantes -= 1
-  puts paquet.length
+  carte_tiree = rand(cartes_restantes)
+  }
+  print flop
 end
