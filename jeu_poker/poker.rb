b4dfbfa186fba680acd75ffffa562f7b64acc5b2
hitman07 <david.malosse@voila.fr>
2015-06-09 14:08:34 +0200

    mise en place tirage des 4 premiers joueurs et leurs valeurs

--- jeu_poker/poker.rb
+++ jeu_poker/poker.rb
@@ -1,17 +1,29 @@
 def poker()
+  #creation paquet
   paquet = []
   couleur_hash = {0 => "coeur", 1 => "carreau", 2 => "pique", 3 => "trefle"}
   (0..3).each {|couleur| (2..14).each {|carte|
-    paquet << [carte, couleur_hash[couleur]]}
-  }
+    paquet << [carte, couleur_hash[couleur]]}}
+  #tirage du flop
   cartes_restantes = 53
   carte_tiree = rand(cartes_restantes)
   flop = []
-  (0..2).each {|flop_carte|
+  (1..3).each {|carte|
   flop << paquet[carte_tiree]
   paquet.delete_at(carte_tiree)
   cartes_restantes -= 1
   carte_tiree = rand(cartes_restantes)
   }
+  #tirage joueurs
+  cartes_joueurs = []
+  (0..7).each {|carte| 
+  cartes_joueurs << paquet[carte_tiree]
+  paquet.delete_at(carte_tiree)
+  cartes_restantes -= 1
+  carte_tiree = rand(cartes_restantes)
+  }
   print flop
+  print cartes_joueurs
+  print paquet.length
+    
 end
